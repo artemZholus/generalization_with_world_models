0c6ef93c04ba9a6ad356c9242611a9f5b0ad0c04
vinnibuh <ivchenkov.yap@phystech.edu>
2022-05-25 20:39:34 +0300

    upd eval script

--- dreamerv2/eval.py
+++ dreamerv2/eval.py
@@ -66,6 +66,8 @@ config = config.update(
     log_every=config.log_every // config.action_repeat,
     time_limit=config.time_limit // config.action_repeat,
     prefill=config.prefill // config.action_repeat)
+if config.segmentation and config.world_model == 'dreamer':
+  config = config.update(img_channels=config.img_channels * 2)
 
 tf.config.experimental_run_functions_eagerly(not config.jit)
 message = 'No GPU found. To actually train on CPU remove this assert.'
@@ -80,6 +82,18 @@ if config.precision == 16:
 print('Logdir', logdir)
 train_replay = common.Replay(logdir / 'train_replay', config.replay_size, **config.replay)
 eval_replay = common.Replay(logdir / 'eval_replay', config.time_limit or 1, **config.replay)
+Async.UID = str(uuid.uuid4().hex)
+atexit.register(Async.close_all)
+
+replays = dict()
+replays["train"] = common.Replay(logdir / 'train_replay', config.replay_size, **config.replay)
+replays["eval"] = common.Replay(logdir / 'eval_replay', config.time_limit or 1, **config.replay)
+tronev = config.train_wm_eval or config.train_ac_eval
+if config.multitask.mode == 'tronev':
+  replays["eval_rand"] = common.Replay(logdir / 'eval_rand_replay', config.time_limit or 1, **config.replay)
+if (config.multitask.mode != 'none') and (config.multitask.mode != 'tronev'):
+  replays["mt"] = common.Replay(mt_path, load=config.keep_ram, **config.replay)
+step = elements.Counter(replays["train"].total_steps)
 # if config.multitask.mode != 'none':
 #   mt_path = pathlib.Path(config.multitask.data_path).expanduser()
 #   mt_replay = common.Replay(mt_path, load=config.keep_ram, **config.replay)
@@ -124,6 +138,15 @@ def make_env(config, mode, **kws):
     )
     env.dump_tasks(str(logdir / 'tasks.pkl'))
     env = common.NormalizeAction(env)
+  elif suite == 'causal':
+    variables_space = dict(train='space_a', eval='space_b')[mode]
+    # params = yaml.safe_load(config.env_params)
+    params = config.cw_params._flat.copy()
+    params.update(kws)
+    env = common.CausalWorld(
+      task, variables_space, config.action_repeat,
+      config.image_size, **params
+    )
   else:
     raise NotImplementedError(suite)
   env = common.TimeLimit(env, config.time_limit)
@@ -140,12 +163,16 @@ def per_episode(ep, mode):
   if 'task_name' in ep:
     task_name = ep['task_name'][0]
   score = float(ep['reward'].astype(np.float64).sum())
+  raw_score = float(ep['raw_reward'].astype(np.float64).sum())
   print(f'{mode.title()} episode has {length} steps and return {score:.1f}.')
-  replay_ = dict(train=train_replay, eval=eval_replay)[mode]
+  # replay_ = dict(train=train_replay, eval=eval_replay)[mode if 'eval' not in mode else 'eval']
+  replay_ = replays[mode]
   if not freezed_replay:
     ep_file = replay_.add(ep)
+    # replays["mt"]._episodes[str(ep_file)] = ep
   logger.scalar(f'{mode}_transitions', replay_.num_transitions)
   logger.scalar(f'{mode}_return', score)
+  logger.scalar(f'{mode}_raw_return', raw_score)
   logger.scalar(f'{mode}_length', length)
   logger.scalar(f'{mode}_eps', replay_.num_episodes)
   prefix = 'eval' if mode == 'eval' else ''
@@ -154,8 +181,9 @@ def per_episode(ep, mode):
     prefix = f'{prefix}_{task_name}'
   summ = {
     f'{config.logging.env_name}/{prefix}_return': score,
-    f'train_env_step': train_replay.num_transitions,
-    f'eval_env_step': eval_replay.num_transitions,
+    f'{config.logging.env_name}/{prefix}_raw_return': raw_score,
+    f'train_env_step': replays["train"].num_transitions,
+    f'eval_env_step': replays["eval"].num_transitions,
     f'{config.logging.env_name}/{prefix}_length': length
   }
   # if config.logging.wdb:
@@ -197,14 +225,14 @@ train_driver = common.Driver(
   partial(make_env, config, 'train'), num_envs=2,
   mode=parallel, lock=config.num_envs > 1, lockfile=config.train_tasks_file,
 )
-task_vec = None
-for env in train_driver._envs:
-  env.randomize_tasks = False
-  if task_vec is None:
-    if config.parallel:
-      task_vec = env.call('get_task_vector')()
-    else:
-      task_vec = env.get_task_vector()
+# task_vec = None
+# for env in train_driver._envs:
+#   env.randomize_tasks = False
+#   if task_vec is None:
+#     if config.parallel:
+#       task_vec = env.call('get_task_vector')()
+#     else:
+#       task_vec = env.get_task_vector()
 train_driver.on_episode(lambda ep: per_episode(ep, mode='train'))
 train_driver.on_step(lambda _: step.increment())
 syncfile = None
@@ -218,18 +246,34 @@ eval_driver = common.Driver(
 for env in eval_driver._envs:
   env.randomize_tasks = False
 eval_driver.on_episode(lambda ep: per_episode(ep, mode='eval'))
-
-prefill = max(0, config.prefill - train_replay.total_steps)
+kind = 'policy'
+def ev_per_ep(ep, mode='eval'):
+  global kind
+  if kind == 'policy':
+    per_episode(ep, mode='eval')
+  elif kind == 'random':
+    per_episode(ep, mode='rand_eval')
+eval_driver.on_episode(ev_per_ep)
+prefill = max(0, config.prefill - replays["train"].total_steps)
+random_agent = common.RandomAgent(action_space)
 if prefill:
   print(f'Prefill dataset ({prefill} steps).')
   random_agent = common.RandomAgent(action_space)
   train_driver(random_agent, episodes=1)
   eval_driver(random_agent, episodes=1)
+  if config.multitask.mode == 'tronev':
+    kind = 'random'
+    eval_driver(random_agent, episodes=1)
+    kind = 'policy'
   train_driver.reset()
   eval_driver.reset()
 freezed_replay = True
 print('Create agent.')
-train_dataset = iter(train_replay.dataset(**config.dataset))
+
+train_dataset = iter(replays["train"].dataset(**config.dataset))
+eval_dataset = iter(replays["eval"].dataset(**config.dataset))
+if tronev:
+  train_rand_dataset = iter(replays["eval_rand"].dataset(**config.dataset))
 # eval_dataset = iter(eval_replay.dataset(**config.dataset))
 
 agnt = agent.Agent(config, logger, action_space, step, train_dataset)
@@ -244,12 +288,12 @@ from collections import defaultdict
 eval_policy = functools.partial(agnt.policy, mode='eval')
 class MyStatsSaver:
   def __init__(self):
-    self.angle = None
+    self.task = None
     self.stats = defaultdict(list)
 
   def on_episode(self, ep, mode='train'):
-    ep_return = sum(ep['reward'])
-    self.stats[self.angle].append(ep_return)
+    ep_return = sum(ep['raw_reward'])
+    self.stats[self.task].append(ep_return)
 
   def dump(self, path):
     stats = {}
@@ -259,27 +303,45 @@ class MyStatsSaver:
       pickle.dump(stats, f)
 my_saver = MyStatsSaver()
 eval_driver.on_episode(my_saver.on_episode)
-env_name = config.task.split('_', 2)[-1]
-env_name = env_name + '-v2'
-if config.parallel:
-  task_set, task_id = eval_driver._envs[0].call('get_task_set', env_name)()
-else:
-  task_set, task_id = eval_driver._envs[0].get_task_set(env_name)
+# env_name = config.task.split('_', 2)[-1]
+# env_name = env_name + '-v2'
+# if config.parallel:
+#   task_set, task_id = eval_driver._envs[0].call('get_task_set', env_name)()
+# else:
+#   task_set, task_id = eval_driver._envs[0].get_task_set(env_name)
 
+def tasks_generator():
+  x_mean = 0.95
+  y_mean = 0.95
+  mass_mean = 0.045
 
-for angle in tqdm(range(0, 360, 5), desc=logdir.stem):
-  curr_task_vec = task_vec.copy()
-  my_saver.angle = angle
-  curr_task_vec[3] = float(angle)
+  for y_size in np.linspace(0.075, 0.115, 10): # 10 vals
+    for mass in np.linspace(0.015, 0.100, 10): # 10 vals
+      yield {'tool_block': {'mass': mass, 'size': np.array([x_mean, y_size, 0.085])}}, \
+        (mass, x_mean, y_size)
+
+  for x_size in np.linspace(0.075, 0.115, 10): # 10 vals
+    for mass in np.linspace(0.015, 0.100, 10): # 10 vals
+      yield {'tool_block': {'mass': mass, 'size': np.array([x_size, y_mean, 0.085])}}, \
+        (mass, x_size, y_mean)
+
+  for x_size in np.linspace(0.075, 0.115, 10): # 13 vals
+    for y_size in np.linspace(0.075, 0.115, 10): # 13 vals
+      yield {'tool_block': {'mass': mass_mean, 'size': np.array([x_size, y_size, 0.085])}}, \
+        (mass_mean, x_size, y_size)
+
+for task_id, task in tqdm(tasks_generator(), desc=logdir.stem):
+  # curr_task_vec = task_vec.copy()
+  my_saver.task = task
   for env in eval_driver._envs:
     if config.parallel:
-      curr_task = env.call('set_task_vector', curr_task_vec)()
-      env.call('set_task_set', env_name, [curr_task])()
+      curr_task = env.call('set_starting_state', task_id, check_bounds=False)()
+      # env.call('set_task_set', env_name, [curr_task])()
     else:
-      curr_task = env.set_task_vector(curr_task_vec)
-      env.set_task_set(env_name, [curr_task])
+      curr_task = env.set_starting_state(task_id, check_bounds=False)
+      # env.set_task_set(env_name, [curr_task])
 
-  eval_driver(eval_policy, episodes=20)
+  eval_driver(eval_policy, episodes=config.eval_episodes_per_env)
   my_saver.dump(logdir / 'stats.pkl')
 
 # while step < config.steps:
