8f693c97e72e1c42de3c877b884905938dd2c401
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-06-22 13:20:27 +0400

    upd

--- common/state_models/__init__.py
+++ common/state_models/__init__.py
@@ -3,9 +3,9 @@ from .reasoner_mlp import ReasonerMLP
 from .influencer import Influencer
 from .dual_no_cond import DualNoCond
 from .rssm_gibbs import RSSM_GIBBS
-from .dual_gibbs import DualGibbs
 from .reasoner_mlp_gibbs import ReasonerMLPGibbs
+from .dual_gibbs import DualGibbs
 
 __all__ = [
-    'RSSM', 'ReasonerMLP', 'Influencer', 'DualNoCond', 'RSSM_GIBBS', 'DualGibbs', 'ReasonerMLPGibbs',
+    'RSSM', 'ReasonerMLP', 'ReasonerMLPGibbs', 'Influencer', 'DualNoCond', 'RSSM_GIBBS', 'DualGibbs',
 ]
